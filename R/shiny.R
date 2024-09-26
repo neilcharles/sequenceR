a6344f8e7bd4219a92f58cce9666453948f4acfc
Neil Charles <neil.d.charles@gmail.com>
2024-09-26 11:30:54 +0100

    added shiny login screen

--- R/shiny.R
+++ R/shiny.R
@@ -25,3 +25,46 @@ seq_shiny_bslib_theme <- function(){
   )
 }
 
+#' Google login screen with a background image
+#'
+#' @param req
+#' @param title
+#' @param bg_image
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' # When calling shiny use...
+#' # shinyApp(gar_shiny_ui(ui, login_ui = seq_google_login_screen()), server)
+seq_google_login_screen <- function(req, title = "App Login", bg_image){
+  login_screen <-  {
+    shiny::addResourcePath("img", system.file("img", package = "googleAuthR"))
+    shiny::addResourcePath("css", system.file("css", package = "googleAuthR"))
+    shiny::fillPage(
+      padding = 50,
+      title = title,
+      shiny::tags$head(shiny::tags$link(rel = "stylesheet",
+                                        href = "css/button.css")),
+
+      shiny::img(
+        src = bg_image,
+        style = '
+        left: 50%;
+        position: absolute;
+        transform: translate(-50%, 0);
+        overflow: hidden;
+        top: 0;
+        z-index: -1;
+          height: 100%;
+    width: 100%;'
+      ),
+
+      shiny::a(
+        href = gar_shiny_auth_url(req),
+        shiny::tags$button(class = "loginBtn loginBtn--google",
+                           "Login with Google")
+      )
+    )
+  }
+}
